a0db32a0938ee0594cf599981f2fa4f00872884e
dunkthelunk <prudhvi.porandla@icloud.com>
2018-08-12 16:21:33 +0530

    fork and wait

--- fork-wait.c
+++ fork-wait.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 int main() {
+  int x = 21;
   pid_t child_pid = fork();
   if(child_pid < 0) {
     perror("fork() error");
@@ -11,14 +12,18 @@ int main() {
   else if (child_pid == 0) { // child_pid is 0 in child process
     printf("Child process - %i\n", getpid()); 
     printf("Parent process - %i\n", getppid()); 
+    ++x;
+    printf("x in child process is %d\n", x); 
     execl("usr/bin/vim", "vim", "fork-wait312.c", NULL); // execl replaces current process
     perror("execl failed"); // printed only if execl fails
   }
   else {
     printf("Main process - %i\n", getpid()); 
     printf("parent of Main process - %i\n", getppid()); 
+    --x;
+    printf("x in parent is %d\n", x);
     wait(NULL);
-    printf("child process exited\n");; 
   }
+  // both will execute below
   return 0;
 }
